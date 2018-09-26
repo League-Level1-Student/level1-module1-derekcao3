da14b2ff8c500086f946b4ce819035b7abf0df48
Cao.Derek <Cao.Derek@SEMS-259-28.SEMS259.LAB>
2018-09-26 16:25:51 -0700

    Binary Converter, Person

---
+++ src/Person.java
@@ -0,0 +1,17 @@
+
+public class Person {
+private String person = "";
+private String power = "";
+public String getName() {
+	return person;
+}
+public String getPower() {
+	return power;
+}
+public void setName(String person) {
+	this.person = person;
+}
+public void setPower(String power) {
+	this.power = power;
+}
+}
---
+++ src/PersonMaker.java
@@ -0,0 +1,17 @@
+
+public class PersonMaker {
+public static void main(String[] args) {
+	Person person = new Person();
+	person.setName("jacopppppp");
+	person.setPower("b0x");
+	System.out.println(person.getName() + " has " + person.getPower() );
+	Person purson = new Person();
+	purson.setName("jacopx");
+	purson.setPower("yo1nk");
+	System.out.println(purson.getName() + " has " + purson.getPower() );
+	Person preson = new Person();
+	preson.setName("bocaj");
+	preson.setPower("x0b");
+	System.out.println(preson.getName() + " sah " + preson.getPower() );
+}
+}
